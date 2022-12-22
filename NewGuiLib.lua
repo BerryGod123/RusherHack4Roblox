fb16bfa3899b46737290c5df58fe7aed8e229287
BerryGod123 <121221697+BerryGod123@users.noreply.github.com>
2022-12-22 18:37:35 +0100

    Update NewGuiLib.lua

--- NewGuiLib.lua
+++ NewGuiLib.lua
@@ -348,7 +348,7 @@ function lib:CreateWindow()
 		tabname.Position = UDim2.new(0, 203,0, 37)
 		tabname.Size = UDim2.new(0, 175, 0, 23)
 		tabname.Font = Enum.Font.SourceSans
-		tabname.Text = "       "..title
+		tabname.Text = "   "..title
 		tabname.TextColor3 = Color3.fromRGB(255, 255, 255)
 		tabname.TextSize = 26.000
 		tabname.TextWrapped = true
