33e0845530612305c8f34474ff0ea4ffa857e621
BerryGod123 <121221697+BerryGod123@users.noreply.github.com>
2022-12-22 17:39:51 +0100

    Update NewGuiLib.lua

--- NewGuiLib.lua
+++ NewGuiLib.lua
@@ -345,7 +345,7 @@ function lib:CreateWindow()
 		tabname.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 		tabname.BackgroundTransparency = 1.000
 		tabname.BorderSizePixel = 0
-		tabname.Position = UDim2.new(0, 199,0, 34)
+		tabname.Position = UDim2.new(0, 199,0, 37)
 		tabname.Size = UDim2.new(0, 145, 0, 13)
 		tabname.Font = Enum.Font.SourceSansBold
 		tabname.Text = "        "..title
